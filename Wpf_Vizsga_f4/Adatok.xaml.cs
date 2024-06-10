4f1f3ae85777f752631bc4e7a5d3c542ba40a029
Szidav <135047885+Szidav@users.noreply.github.com>
2024-06-10 20:04:16 +0200

    Add files via upload

---
+++ Wpf_Vizsga_f4/Adatok.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Console_feladat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf_Vizsga_f4
+{
+    /// <summary>
+    /// Interaction logic for Adatok.xaml
+    /// </summary>
+    public partial class Adatok : Window
+    {
+        //A projekt dependenciesnél Add Project Ref-nél  hozzácsatolható a Console feladat.
+        List<Dolgozo> dolgozok = new List<Dolgozo>();
+
+        public Adatok(List<Dolgozo> adatok)
+        {
+            InitializeComponent();
+            dolgozok = adatok;
+            datagridDolgozok.ItemsSource = dolgozok;
+        }
+    }
+}
---
+++ Wpf_Vizsga_f4/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Console_feladat;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Vizsga_f4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Dolgozo> dolgozok = new List<Dolgozo>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonMegnyit_Click(object sender, RoutedEventArgs e)
+        {
+            Megnyitas();
+
+            //Másik mód ha nem teszem külön metódusba!!!
+            
+            /*OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == true)
+            {
+                try
+                {
+                    var sorok = File.ReadAllLines(dialog.FileName, Encoding.Default);
+
+                    for (int i = 1; i < sorok.Length; i++)
+                    {
+                        dolgozok.Add(new Dolgozo(sorok[i]));
+                    }
+                    datagridDolgozok.ItemsSource = dolgozok;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }*/
+        }
+
+        //Metódusos a megoldás, a kód duplikálás elkerülése véget.
+        //Előfordulhat rendezés.
+        private void Megnyitas()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == true)
+            {
+                try
+                {
+                    var sorok = File.ReadAllLines(dialog.FileName, Encoding.Default);
+
+                    for (int i = 1; i < sorok.Length; i++)
+                    {
+                        dolgozok.Add(new Dolgozo(sorok[i]));
+                    }
+                    datagridDolgozok.ItemsSource = dolgozok;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+
+        private void menuitemMegnyitás_Click(object sender, RoutedEventArgs e)
+        {
+            Megnyitas();
+            Adatok adatok = new Adatok(dolgozok);
+
+            //Megjelenéshez kell a ShowDialog()
+            adatok.ShowDialog();
+        }
+    }
+}
